213f0025a4f2af4283c6e032a3db90f9216010e7
Adam Jordens <adam@jordens.org>
2011-05-04 16:44:11 -0700

    - Added 'registration' model

--- log/models.py
+++ log/models.py
@@ -6,3 +6,7 @@ class Entry(models.Model):
     created_at = models.DateTimeField(auto_now_add = True)
     updated_at = models.DateTimeField(auto_now = True)
 
+class Registration(models.Model):
+    guid = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
