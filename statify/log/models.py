a6f1e229008996c03ef21a1412f7a101ca2904c8
Adam Jordens <adam@jordens.org>
2011-05-04 13:27:24 -0700

    - Created 'log' app - Initial 'log_entry' model

---
+++ statify/log/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Entry(models.Model):
+    guid = models.CharField(max_length=255)
+    entry = models.TextField()
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+
